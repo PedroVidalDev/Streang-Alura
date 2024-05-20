d7765eb35241e58f863891d4d0683600327c50f2
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-19 21:00:57 -0300

    feat: adicionando configuracoes de cors ao projeto

--- src/main/java/com/pedro/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/pedro/screenmatch/ScreenmatchApplication.java
@@ -1,14 +1,5 @@
 package com.pedro.screenmatch;
 
-import com.pedro.screenmatch.model.DadosEpisodio;
-import com.pedro.screenmatch.model.DadosSerie;
-import com.pedro.screenmatch.model.DadosTemporada;
-import com.pedro.screenmatch.principal.Principal;
-import com.pedro.screenmatch.repository.SerieRepository;
-import com.pedro.screenmatch.service.ConsumoApi;
-import com.pedro.screenmatch.service.ConverteDados;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
